5b051876a7a540d4c3c7680f6e752f304c866629
juaj <juaj@itu.dk>
2024-02-07 09:31:26 +0100

    relative path

--- MAE.py
+++ MAE.py
@@ -2,14 +2,14 @@ import pandas as pd
 import pyarrow.parquet as pq
 import numpy as np
 
-file_path_predicted = 'C:\\Users\\Julie Abildgaard\\OneDrive\\ITU\\Kandidat\\Code\\master-thesis-nina-julie\\data\\target_and_predicted\\US-CAL-BANC_predicted.parquet'
+file_path_predicted = 'data\\target_and_predicted\\US-CAL-BANC_predicted.parquet'
 table = pq.read_table(file_path_predicted)
 #df.to_csv('filename.csv')
 df_predicted = table.to_pandas()
 print(df_predicted.head)
 
 
-file_path_target = 'C:\\Users\\Julie Abildgaard\\OneDrive\\ITU\\Kandidat\\Code\\master-thesis-nina-julie\\data\\target_and_predicted\\US-CAL-BANC_target.parquet'
+file_path_target = 'data\\target_and_predicted\\US-CAL-BANC_target.parquet'
 table = pq.read_table(file_path_target)
 df_target = table.to_pandas()
 print(df_target.head)
